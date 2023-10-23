ee94217d4348d77c971d2a69d4f0bd186163eb43
tommy <you@example.com>
2023-10-23 22:19:49 +0800

    submit to git

--- src/classes/Pool.ts
+++ src/classes/Pool.ts
@@ -1,7 +1,9 @@
 import {ParsedPool, RawPool} from "../types/pool";
-import {TokenPair} from "../tokens/token";
+import {Token, TokenPair} from "../tokens/token";
 import {bnToOptionType} from "../utils/conversions";
 import {toHex} from "../utils/utils";
+import {OptionType} from "../types/options";
+import {getMultipleTokensValueInUsd} from "../tokens/tokenPrices";
 
 type Props = |{raw:RawPool}|{parsed:ParsedPool}
 
@@ -12,9 +14,9 @@ export class Pool{
     public id:string
     constructor(props:Props) {
         if("raw" in props){
-            this.raw = this.props.raw;
+            this.raw = props.raw;
             this.parsed = this.parseFromRow(props.raw);
-            this.id=this.generateId();
+            this.id=this.generatedId();
         }
     }
 
@@ -27,5 +29,45 @@ export class Pool{
     }
     generatedId():string{
         return JSON.stringify(this.parsed,Object.keys(this.parsed).sort());
+    }
+    eq(other:Pool):boolean{
+        return this.id === other.id;
+    }
+    isType(type:OptionType):boolean{
+        return this.parsed.optionType === type;
+    }
+    async tokenPricesInUsd(){
+        const [base,quote]=await getMultipleTokensValueInUsd([
+            this.tokenPair.base.id,
+            this.tokenPair.quote.id
+        ]);
+        return {base,quote};
+    }
+
+    //Getters
+    get typeAsText():string{
+        return this.parsed.optionType === OptionType.Call ? "Call":"Put";
+    }
+    get isCall():boolean{
+        return this.parsed.optionType === OptionType.Call;
+    }
+    get isPut():boolean{
+        return this.parsed.optionType === OptionType.Put;
+    }
+    get underlying():Token{
+        return this.isCall? this.tokenPair.base : this.tokenPair.quote;
+    }
+    get digits():number{
+        return this.underlying.decimals;
+    }
+    get tokenAddress(): string {
+        return this.underlying.tokenAddress;
+    }
+
+    get symbol(): string {
+        return this.underlying.symbol;
+    }
+    get name():string{
+        return `${this.tokenPair.base.symbol}/${this.tokenPair.quote.symbol} ${this.typeAsText} Pool (${this.symbol})`
     }
 }
---
+++ src/tokens/tokenPrices.ts
@@ -0,0 +1,52 @@
+import {TokenKey} from "./token";
+type Coin = {
+    id: TokenKey;
+    current_price:number;
+}
+type RecentValue={
+    id:TokenKey;
+    timestamp:number;
+    value:number;
+}
+const getMulticoinUrl = (ids:TokenKey[],currency="usd")=>{
+    const composedIds = ids.reduce(
+        (acc,cur,i)=>(i===0?cur:acc+"%2C"+cur),""
+    );
+    return `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${composedIds}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`;
+}
+const getMultipleCoinsInUsd = async (ids: TokenKey[]): Promise<number[]> =>
+    fetch(getMulticoinUrl(ids))
+        .then((response) => response.json())
+        .then((data) =>
+            ids.map((id) =>
+                    data.find(
+                        (coin: Coin) => coin.id === id
+                    )
+                ).map((coin:Coin) => coin.current_price)
+        );
+const isFresh = (recent:RecentValue)=>recent.timestamp+freshTime>Date.now();
+const tokenValueCache = new Map<TokenKey,RecentValue>();
+const checkCache = (id: TokenKey): number | undefined => {
+    const fromCache = tokenValueCache.get(id);
+
+    if (fromCache && isFresh(fromCache)) {
+        return fromCache.value;
+    }
+};
+
+
+export const getMultipleTokensValueInUsd = async (ids:TokenKey[]):Promise<number[]>=>{
+    const fromCache = ids.map((id) => checkCache(id));
+    if(fromCache.every(Boolean)){
+        return fromCache as number[];
+    }
+    const values = await getMultipleCoinsInUsd(ids);
+    const timestamp = Date.now();
+    ids.forEach((id,index)=>{
+        tokenValueCache.set(id,{value:values[index],timestamp,id});
+    })
+    return values;
+}
+
+
+const freshTime = 30000;
